136bbd3d62d6be38c1fb26ba219e03578cc5ece3
devjohn42 <devjohncb@gmail.com>
2026-03-30 20:19:34 -0300

    :lipstick: feat: implements tailwindcss and create hero component in the landing page
    - Add images
    - Configure tailwindcss
    - Implement globals styles
    - Create the hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Hero } from './_components/hero'
+
+{
+	/* <h1 className="font-extrabold text-[#7D74DE]">ESports Web</h1> */
+}
+
 function App() {
-	return <h1>ESports Web</h1>
+	return (
+		<div className="max-w-336 mx-auto flex flex-col items-center my-20">
+			<Hero />
+		</div>
+	)
 }
 
 export default App
---
+++ src/_components/hero.tsx
@@ -0,0 +1,67 @@
+import logo from '../assets/logo-nlw.svg'
+
+export function Hero() {
+	return (
+		<>
+			<img src={logo} alt="" />
+			<h1 className="text-[64px] text-[#7D74DE] font-black mt-20">
+				Seu{' '}
+				<span className="bg-(image:--text-gradient) bg-clip-text text-transparent">
+					duo
+				</span>{' '}
+				está aqui!
+			</h1>
+
+			<div className="grid grid-cols-6 gap-6 mt-16">
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-1.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">Counter Strike</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-2.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">Dota 2</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 2 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-3.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">WarCraft</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 7 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-4.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">League Of Legends</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 4 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-5.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">Fortnite</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 9 anúncios</span>
+					</div>
+				</a>
+				<a href="" className="relative rounded-lg overflow-hidden">
+					<img src="/game-6.png" alt="" />
+
+					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+						<strong className="font-bold text-zinc-50 block">Apex Legends</strong>
+						<span className="text-zinc-300 text-sm mt-4"> 2 anúncios</span>
+					</div>
+				</a>
+			</div>
+		</>
+	)
+}
