d2307e7f4a52718b8481c20db2f3888ee5cc8a45
devjohn42 <devjohncb@gmail.com>
2026-04-12 17:19:34 -0300

    :sparkles: feat: consume the games from the API

--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react'
 import logo from '../assets/logo-nlw.svg'
 
+interface GameBannerProps {
+	bannerUrl: string
+	title: string
+	adsCount: number
+}
+
+interface Game {
+	id: string
+	title: string
+	bannerUrl: string
+	_count: {
+		ads: number
+	}
+}
+
+const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
+	return (
+		<a href="" className="relative rounded-lg overflow-hidden">
+			<img src={bannerUrl} alt="" />
+
+			<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
+				<strong className="font-bold text-zinc-50 block">{title}</strong>
+				<span className="text-zinc-300 text-sm mt-4"> {adsCount} anúncio(s)</span>
+			</div>
+		</a>
+	)
+}
+
 export function Hero() {
+	const [games, setGames] = useState<Game[]>([])
+
+	useEffect(() => {
+		fetch('http://localhost:3333/games')
+			.then((response) => response.json())
+			.then((data) => {
+				setGames(data)
+			})
+	}, [])
+
 	return (
 		<section className="flex flex-col items-center">
 			<img src={logo} alt="" />
@@ -13,54 +52,16 @@ export function Hero() {
 			</h1>
 
 			<div className="grid grid-cols-6 gap-6 mt-16">
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-1.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">Counter Strike</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 4 anúncios</span>
-					</div>
-				</a>
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-2.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">Dota 2</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 2 anúncios</span>
-					</div>
-				</a>
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-3.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">WarCraft</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 7 anúncios</span>
-					</div>
-				</a>
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-4.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">League Of Legends</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 4 anúncios</span>
-					</div>
-				</a>
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-5.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">Fortnite</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 9 anúncios</span>
-					</div>
-				</a>
-				<a href="" className="relative rounded-lg overflow-hidden">
-					<img src="/game-6.png" alt="" />
-
-					<div className="w-full pt-16 pb-4 px-4 bg-(image:--game-gradient) absolute bottom-0 left-0 right-0">
-						<strong className="font-bold text-zinc-50 block">Apex Legends</strong>
-						<span className="text-zinc-300 text-sm mt-4"> 2 anúncios</span>
-					</div>
-				</a>
+				{games.map((game) => {
+					return (
+						<GameBanner
+							key={game.id}
+							title={game.title}
+							bannerUrl={game.bannerUrl}
+							adsCount={game._count.ads}
+						/>
+					)
+				})}
 			</div>
 		</section>
 	)
